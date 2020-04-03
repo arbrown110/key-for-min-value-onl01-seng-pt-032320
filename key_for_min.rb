41b62a87cd8c82382e1ff18386ff8f30451bd1b7
chic-caller-7520 <arbrown110@gmail.com>
2020-04-03 15:36:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,10 @@ def key_for_min_value(name_hash)
     if  small_value == nil || value < small_value
       small_value = value
       small_key = key
-      
+    end 
+  end 
+  
+  small_key
   
 
 end
