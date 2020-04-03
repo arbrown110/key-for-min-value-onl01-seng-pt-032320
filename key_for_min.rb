0f8b69aa0364568723a42ca7e1e0c3601d1463e6
chic-caller-7520 <arbrown110@gmail.com>
2020-04-03 15:36:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   small_key = nil 
   small_value = nil
-  name_hash.eah do | key, value |
+  name_hash.each do | key, value |
     if  small_value == nil || value < small_value
       small_value = value
       small_key = key
